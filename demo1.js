50b5e38f8ea9dc30406149ec25b50dec0484d834
your new username <smallmonster900@gmail.com>
2024-06-26 20:47:31 +0700

    a

---
+++ demo1.js
@@ -0,0 +1,27 @@
+alert("Xin chào mình js");
+
+// ngôn ngữ lập trình javascript 
+
+// + Biến, Hằng 
+// + Kiểu dữ liệu
+// + Toán tử 
+// + câu lệnh rẽ nhánh (if, if-else,switch-case)
+// + vòng lặp (for,while, do-while)
+// + mảng 
+
+// thường được sử dụng cho website 
+// Tăng tương tác người dùng 
+// thay đổi nội dung HTML 
+// thay đổi CSS 
+
+// ===> JS 
+    // Game 
+    // backend nodejs
+    // web frontend (reactjs, angular ,...)
+    // mobile (react native)
+
+// 3 cách để nhúng js vào trang html 
+// cách 1: nhúng vào thẻ html 
+// cách 2: nhúng mã javascript ở trong thẻ <script>
+// cách 3: nhúng tệp đuôi mở rộng là .js vào webstie  
+
